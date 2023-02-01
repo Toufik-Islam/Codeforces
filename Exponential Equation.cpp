1e09bb0736e2f6b980c64581adca1db1e78d3090
Toufik Islam <112005587+Toufik-Islam@users.noreply.github.com>
2023-02-02 00:05:21 +0600

    Exponential Equation.. 2 feb 2023

---
+++ Exponential Equation.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        if(n%2==1)
+            cout << "-1\n";
+        else
+            cout << n/2 << " 1\n";
+    }
+
+    return 0;
+}
