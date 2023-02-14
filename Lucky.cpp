52d9d6db024ce4413855275192e10bfb2bad3e51
Toufik Islam <112005587+Toufik-Islam@users.noreply.github.com>
2023-02-14 15:59:05 +0600

    Lucky.. 14 feb 2023

---
+++ Lucky.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string s;
+        cin >> s;
+        if(s[0]+s[1]+s[2]==s[3]+s[4]+s[5])
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
+
