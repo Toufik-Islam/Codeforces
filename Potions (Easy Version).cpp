17ad32140006a457e78cd2574743c94188f24c5a
Toufik Islam <112005587+Toufik-Islam@users.noreply.github.com>
2023-02-21 15:38:23 +0600

    potions easy version.. 21 feb 2023

---
+++ Potions (Easy Version).cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    long long sum=0;
+    priority_queue<int,vector<int>, greater<int>> pq;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin >>x;
+        pq.push(x);
+        sum+=x;
+        if(sum<0)
+        {
+            sum-=pq.top();
+            pq.pop();
+        }
+    }
+    cout << pq.size();
+    return 0;
+}
+
